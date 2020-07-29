6d282267f83ed8835ac56ae219e8ecc79573be6c
Andrius Martini <andrius.martini1994@gmail.com>
2020-07-28 22:45:37 -0300

    Turn slider infinity

--- src/pages/NewCategory/index.jsx
+++ src/pages/NewCategory/index.jsx
@@ -8,6 +8,18 @@ const NewCategory = () => {
     <Template>
       <h1>Cadastro de Categoria</h1>
 
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input type='text' />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
       <Link to='/'>
         Ir para Home
       </Link>
